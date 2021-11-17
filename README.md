550ba37538f011e5d38651d77dbf0a78b148319d
Bostjan Stupica <bostjan@stupica.com>
2021-11-17 23:14:41 +0100

    Import

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # javaBCacheServer
 Java (B)Cache server > to serve/centralize cache.
+
+
+# Reference
+
+* https://cache2k.org/benchmarks.html
+* https://cruftex.net/2017/09/01/Java-Caching-Benchmarks-Part-3.html
+* https://infinispan.org/blog/tags/benchmarks/
+* https://redis.io/topics/benchmarks
---
+++ java/bCacheServer/run01.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This ..
+#
+
+PROG_DIR=.
+#PROG_DIR=target
+PATH_LOG=$PATH_PROG/log
+
+LOG_CONF=$PATH_PROG/properties/logging.properties
+
+VMparam="-server -Xms264m -Xmx986m"
+VMparam="-Djava.util.logging.config.file=$LOG_CONF $VMparam"
+VMparam="-Dapp.path.config=/home/lenko12/bin/lenkoTrader/properties $VMparam"
+
+java $VMparam -jar $PATH_PROG/bCacheServer-0.1.0.jar -v
+
+echo That_s it.
+
+exit 0
