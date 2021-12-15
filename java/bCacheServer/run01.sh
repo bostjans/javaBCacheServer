7903315fb1bad2e3533d96bd63d43042d7f8f389
Bostjan Stupica <bostjan@stupica.com>
2021-12-15 22:27:12 +0100

    Update: class loading;

--- java/bCacheServer/run01.sh
+++ java/bCacheServer/run01.sh
@@ -12,6 +12,8 @@ LOG_CONF=$PATH_PROG/properties/logging.properties
 VMparam="-server -Xms264m -Xmx986m"
 VMparam="-Djava.util.logging.config.file=$LOG_CONF $VMparam"
 #VMparam="-Dapp.path.config=/home/bcache/bin/bCache/properties $VMparam"
+#VMparam="-Djava.rmi.server.codebase=file:///home/bcache/bin/bCache/dbModel-1.7.0.jar $VMparam"
+#VMparam="-Djava.rmi.server.useCodebaseOnly=false $VMparam"
 
 java $VMparam -jar $PATH_PROG/bCacheServer-0.3.1.jar -v
 
