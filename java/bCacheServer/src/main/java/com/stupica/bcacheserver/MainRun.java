060f63f7aef64b2fe449559cc93f61eff748c4c7
Bostjan Stupica <bostjan@stupica.com>
2021-12-29 22:18:32 +0100

    Fix: method cleanup() in lib.;

--- java/bCacheServer/src/main/java/com/stupica/bcacheserver/MainRun.java
+++ java/bCacheServer/src/main/java/com/stupica/bcacheserver/MainRun.java
@@ -56,7 +56,7 @@ public class MainRun extends MainRunBase {
     public static void main(String[] a_args) {
         // Initialization
         GlobalVar.getInstance().sProgName = "bCacheServer";
-        GlobalVar.getInstance().sVersionBuild = "025";
+        GlobalVar.getInstance().sVersionBuild = "026";
         // Ref.: https://stackoverflow.com/questions/6314285/java-util-logging-stops-working-after-a-while
         logger = Logger.getLogger(GlobalVar.getInstance().sProgName);
 
